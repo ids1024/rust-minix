28a69838037b7c3460cfee6a2bd0430a9e5d7e1e
Ian Douglas Scott <ian@iandouglasscott.com>
2019-02-16 15:42:03 -0800

    Use lld

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+if [ -z "$MINIX_ROOT" ]; then
+	echo "\$MINIX_ROOT must be set."
+	exit 1
+fi
+
 export XARGO_RUST_SRC=$PWD/rust/src
 export RUST_TARGET_PATH=$PWD
+export RUSTFLAGS=-L$MINIX_ROOT/usr/lib
+
 XARGO="rustup run nightly cargo run --manifest-path $PWD/xargo/Cargo.toml --"
 
 git submodule update --init rust libc xargo
