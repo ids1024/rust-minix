3d9ab25c51544cf93ad204fb45f6caa212206961
Ian Douglas Scott <ian@iandouglasscott.com>
2019-02-17 11:44:44 -0800

    Use clang for linked
    Although lld is fun, this is more reliable, and clang is needed anyway
    for compiling C libraries

--- build.sh
+++ build.sh
@@ -2,43 +2,22 @@
 
 set -e
 
-if [ -z "$MINIX_ROOT" ]; then
-	echo "\$MINIX_ROOT must be set."
+if [ -z "$MINIX_TOOLDIR" ]; then
+	echo "\$MINIX_TOOLDIR must be set."
 	exit 1
 fi
 
-LIB="$MINIX_ROOT/usr/lib"
+if [ -z "$MINIX_SYSROOT" ]; then
+	echo "\$MINIX_SYSROOT must be set."
+	exit 1
+fi
+
 XARGO="rustup run nightly cargo run --manifest-path $PWD/xargo/Cargo.toml --"
 
 export XARGO_RUST_SRC=$PWD/rust/src
 export XARGO_HOME=$PWD/.xargo
 export RUST_TARGET_PATH=$PWD
-
-link_arg() {
-	echo "\"-C\", \"link-arg=$1\","
-}
-
-pre_link_arg() {
-	echo "\"-Z\", \"pre-link-arg=$1\","
-}
-
-cargo_config() {
-	echo "[target.i586-unknown-minix]"
-	echo "rustflags = ["
-	link_arg "-L=$LIB"
-	link_arg "-lc"
-	link_arg "--dynamic-linker=/usr/libexec/ld.elf_so"
-	link_arg "--no-rosegment"
-	for i in crt0.o crti.o crtbegin.o; do
-		pre_link_arg "$LIB/$i"
-	done
-	for i in crtend.o crtn.o; do
-		link_arg "$LIB/$i"
-	done
-	echo "]"
-}
-
-cargo_config > .cargo/config
+export PATH=$PWD/bin:$MINIX_TOOLDIR/bin:$PATH
 
 git submodule update --init rust libc xargo
 git -C rust submodule update --init src/stdsimd
