5c7656644aa0469d1ec78ce44e7a6884a1fa62ee
Ian Douglas Scott <ian@iandouglasscott.com>
2019-02-17 11:59:07 -0800

    Use minix clang/ar

--- build.sh
+++ build.sh
@@ -18,6 +18,8 @@ export XARGO_RUST_SRC=$PWD/rust/src
 export XARGO_HOME=$PWD/.xargo
 export RUST_TARGET_PATH=$PWD
 export PATH=$PWD/bin:$MINIX_TOOLDIR/bin:$PATH
+export CC_i586_unknown_minix=i586-elf32-minix-clang
+export AR_i586_unknown_minix=i586-elf32-minix-ar
 
 git submodule update --init rust libc xargo
 git -C rust submodule update --init src/stdsimd
