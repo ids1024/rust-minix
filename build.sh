8166d864c066bfbd7e6cc97b3677d1599f7d3073
Ian Douglas Scott <ian@iandouglasscott.com>
2019-02-17 00:28:24 -0800

    lld linking

--- build.sh
+++ build.sh
@@ -1,15 +1,44 @@
 #!/bin/sh
 
+set -e
+
 if [ -z "$MINIX_ROOT" ]; then
 	echo "\$MINIX_ROOT must be set."
 	exit 1
 fi
 
-export XARGO_RUST_SRC=$PWD/rust/src
-export RUST_TARGET_PATH=$PWD
-export RUSTFLAGS=-L$MINIX_ROOT/usr/lib
-
+LIB="$MINIX_ROOT/usr/lib"
 XARGO="rustup run nightly cargo run --manifest-path $PWD/xargo/Cargo.toml --"
+
+export XARGO_RUST_SRC=$PWD/rust/src
+export XARGO_HOME=$PWD/.xargo
+export RUST_TARGET_PATH=$PWD
+
+link_arg() {
+	echo "\"-C\", \"link-arg=$1\","
+}
+
+pre_link_arg() {
+	echo "\"-Z\", \"pre-link-arg=$1\","
+}
+
+cargo_config() {
+	echo "[target.i586-unknown-minix]"
+	echo "rustflags = ["
+	link_arg "-L=$LIB"
+	link_arg "-lc"
+	link_arg "--dynamic-linker=/usr/libexec/ld.elf_so"
+	link_arg "--no-rosegment"
+	for i in crt0.o crti.o crtbegin.o; do
+		pre_link_arg "$LIB/$i"
+	done
+	for i in crtend.o crtn.o; do
+		link_arg "$LIB/$i"
+	done
+	echo "]"
+}
+
+cargo_config > .cargo/config
 
 git submodule update --init rust libc xargo
 git -C rust submodule update --init src/stdsimd
