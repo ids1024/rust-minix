6d66e31a404e68625468e6281f72407a0a96b5f3
Ian Douglas Scott <ian@iandouglasscott.com>
2019-02-17 12:53:41 -0800

    Add script for building libc-test

---
+++ build-libc-test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+. ./env.sh
+
+cd libc/libc-test
+$XARGO test --target i586-unknown-minix
--- build.sh
+++ build.sh
@@ -2,24 +2,7 @@
 
 set -e
 
-if [ -z "$MINIX_TOOLDIR" ]; then
-	echo "\$MINIX_TOOLDIR must be set."
-	exit 1
-fi
-
-if [ -z "$MINIX_SYSROOT" ]; then
-	echo "\$MINIX_SYSROOT must be set."
-	exit 1
-fi
-
-XARGO="rustup run nightly cargo run --manifest-path $PWD/xargo/Cargo.toml --"
-
-export XARGO_RUST_SRC=$PWD/rust/src
-export XARGO_HOME=$PWD/.xargo
-export RUST_TARGET_PATH=$PWD
-export PATH=$PWD/bin:$MINIX_TOOLDIR/bin:$PATH
-export CC_i586_unknown_minix=i586-elf32-minix-clang
-export AR_i586_unknown_minix=i586-elf32-minix-ar
+. ./env.sh
 
 git submodule update --init rust libc xargo
 git -C rust submodule update --init src/stdsimd
